eee63f80aa10917863b0dce6c575c76b8c82c2bd
Daniel1835 <danielirwansyah1@gmail.com>
2022-11-06 01:52:02 -0700

    Initial commit

---
+++ app/src/main/java/com/f55121075_daniel_irwansyah/ListView/Daniel.java
@@ -0,0 +1,14 @@
+package com.f55121075_daniel_irwansyah.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Daniel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daniel);
+    }
+}
---
+++ app/src/main/java/com/f55121075_daniel_irwansyah/ListView/Gatot_SoebrotoActivity.java
@@ -0,0 +1,14 @@
+package com.f55121075_daniel_irwansyah.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gatot_SoebrotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gatot_soebroto);
+    }
+}
